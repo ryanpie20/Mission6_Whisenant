ca57056b2a2dcd7cc3dc9a24797d25111d86aab9
ryanwhisenant <ryanpie@gmail.com>
2024-02-16 18:15:11 -0700

    database allows null for subcategory

--- Mission6_Whisenant/Models/MovieCollection.cs
+++ Mission6_Whisenant/Models/MovieCollection.cs
@@ -3,21 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Mission6_Whisenant.Models
 {
-    //public class MovieCollection
-    //{
-    //    [Key]
-    //    public int MovieID { get; set; }
-    //    public string Category { get; set; }
-    //    public string Title { get; set; }
-    //    public int Year { get; set; }
-    //    public string Director { get; set; }
-    //    public string Rating { get; set; }
-    //    public bool? Edited { get; set; }
-    //    public string? LentTo { get; set; }
-    //    public string? Notes { get; set; }
 
-
-    //}
 
     public class MovieCollection
     {
